c186dfa87710072abd0c2662d5d28f7aff612da2
1josee <trnnhattien@gmail.com>
2025-05-17 23:20:08 +0700

    Initial commit

---
+++ src/test/java/com/example/TTECHT/TtechtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TTECHT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtechtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
