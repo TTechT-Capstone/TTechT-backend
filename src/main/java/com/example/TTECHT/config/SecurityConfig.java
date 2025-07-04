915a8012514568c9e3d7b124d646d491d7ea215e
{username} <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2025-07-04 11:20:17 +0700

    fix: add deploy url for cors

--- src/main/java/com/example/TTECHT/config/SecurityConfig.java
+++ src/main/java/com/example/TTECHT/config/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000","https://ttecht-backend.onrender.com"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
